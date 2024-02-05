54984667a6e7b41e656055963000baabcc952596
Nicolas Frisby <nick.frisby@iohk.io>
2024-02-05 15:12:31 -0800

    WIP drafting GSM tests

--- ouroboros-consensus-diffusion/test/consensus-test/Main.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import qualified Test.Consensus.GSM
 import qualified Test.Consensus.Genesis.Tests (tests)
 import qualified Test.Consensus.HardFork.Combinator (tests)
 import qualified Test.Consensus.Node (tests)
@@ -20,4 +21,5 @@ tests =
           ]
       ]
   , Test.Consensus.Genesis.Tests.tests
+  , testGroup "GSM" Test.Consensus.GSM.tests
   ]
---
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/GSM.hs
@@ -0,0 +1,6 @@
+module Test.Consensus.GSM (tests) where
+
+import           Test.Tasty
+
+tests :: [TestTree]
+tests = []
