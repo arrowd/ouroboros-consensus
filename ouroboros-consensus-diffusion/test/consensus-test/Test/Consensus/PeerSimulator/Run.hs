b56fc7fcd916ef4602e32c774b36643e682f7a20
Armando Santos <armandoifsantos@gmail.com>
2024-02-13 09:54:08 +0000

    fx

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/Run.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/PeerSimulator/Run.hs
@@ -182,7 +182,7 @@ startChainSyncConnectionThread registry tracer cfg activeSlotCoefficient chainDb
 -- | Start the BlockFetch client, using the supplied 'FetchClientRegistry' to
 -- register it for synchronization with the ChainSync client.
 startBlockFetchConnectionThread ::
-  (IOLike m, MonadTime m) =>
+  (IOLike m, MonadTime m, MonadTimer m) =>
   ResourceRegistry m ->
   FetchClientRegistry PeerId (Header TestBlock) TestBlock m ->
   ControlMessageSTM m ->
